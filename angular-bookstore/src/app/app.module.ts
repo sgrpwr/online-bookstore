24290d1e3998401950824f815d0e42c1f623cb00
sagar <pawarsgr18@gmail.com>
2020-03-28 23:00:16 +0530

    Created angular project front end feature 1.

---
+++ angular-bookstore/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- onlinebookstore/src/main/java/in/sagarpawar/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/in/sagarpawar/onlinebookstore/repository/BookRepository.java
@@ -1,8 +1,11 @@
 package in.sagarpawar.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 import in.sagarpawar.onlinebookstore.entity.Book;
 
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long>{
 	
 //	@RestResource(path = "categoryid")
